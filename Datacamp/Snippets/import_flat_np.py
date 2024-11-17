d7914de9879c492d69cc5015c4fdbf48e1fb55a5
julianostefano <110472202+julianostefano@users.noreply.github.com>
2024-11-17 20:15:54 +0000

    Loading a pickled file There are a number of datatypes that cannot be saved easily to flat files, such as lists and dictionaries. If you want your files to be human readable, you may want to save them as text files in a clever manner. JSONs, which you will see in a later chapter, are appropriate for Python dictionaries.
    However, if you merely want to be able to import them into Python, you can serialize them. All this means is converting the object into a sequence of bytes, or a bytestream.
    
    In this exercise, you'll import the pickle package, open a previously pickled data structure from a file and load it.

--- Datacamp/Snippets/import_flat_np.py
+++ Datacamp/Snippets/import_flat_np.py
@@ -3,10 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Assign filename to variable: file
-file = 'digits.csv'
+file = "digits.csv"
 
 # Load file as array: digits
-digits = np.loadtxt(file, delimiter=',')
+digits = np.loadtxt(file, delimiter=",")
 
 # Print datatype of digits
 print(type(digits))
@@ -16,5 +16,40 @@ im = digits[21, 1:]
 im_sq = np.reshape(im, (28, 28))
 
 # Plot reshaped data (matplotlib.pyplot already loaded as plt)
-plt.imshow(im_sq, cmap='Greys', interpolation='nearest')
+plt.imshow(im_sq, cmap="Greys", interpolation="nearest")
+plt.show()
+
+
+# Import numpy
+import numpy as np
+
+# Assign the filename: file
+file = "digits_header.txt"
+
+# Load the data: data
+data = np.loadtxt(file, delimiter="\t", skiprows=1, usecols=[0, 2])
+
+# Print data
+print(data)
+
+
+# Assign filename: file
+file = 'seaslug.txt'
+
+# Import file: data
+data = np.loadtxt(file, delimiter='\t', dtype=str)
+
+# Print the first element of data
+print(data[0])
+
+# Import file as floats and skip the first row: data_float
+data_float = np.loadtxt(file, delimiter='\t', dtype=float, skiprows=1)
+
+# Print the 10th element of data_float
+print(data_float[9, :])
+
+# Plot a scatterplot of the data
+plt.scatter(data_float[:, 0], data_float[:, 1])
+plt.xlabel('time (min.)')
+plt.ylabel('percentage of larvae')
 plt.show()
