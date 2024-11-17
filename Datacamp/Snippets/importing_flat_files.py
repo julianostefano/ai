20542b553811242189139ba1c4c004f66e75cd89
julianostefano <110472202+julianostefano@users.noreply.github.com>
2024-11-17 16:45:27 +0000

    Using NumPy to import flat files In this exercise, you're now going to load the MNIST digit recognition dataset using the numpy function loadtxt() and see just how easy it can be:
    The first argument will be the filename.
    The second will be the delimiter which, in this case, is a comma.
    The MNIST dataset is a collection of handwritten digits from 0 to 9, frequently used in the field of machine learning.
     It serves as a benchmark for evaluating algorithm performance in recognizing and classifying these numbers.

---
+++ Datacamp/Snippets/importing_flat_files.py
@@ -0,0 +1,12 @@
+# Open a file as read-only and bind it to file
+with open('moby_dick.txt', mode='r') as file:
+  	# Print it
+    print(file.read())
+
+
+# Read & print the first 3 lines
+with open('moby_dick.txt') as file:
+    print(file.readline())
+    print(file.readline())
+    print(file.readline())
+
